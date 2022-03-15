4248e008ad5af2912188f7bfc9157e7abea66e60
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-03-15 15:57:12 -0300

    🚀 :rocket: [Add] JWT Settings

---
+++ Settings/JWTSettings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseManager.Settings
+{
+    public class JWTSettings
+    {
+        public string Secret {get; set; }
+    }
+}
