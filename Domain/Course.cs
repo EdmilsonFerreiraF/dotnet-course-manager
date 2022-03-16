3659f2d984a18aada4f705d75031178d07e63ead
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-03-16 12:54:30 -0300

    🚀 :rocket: [Add] Course domain

---
+++ Domain/Course.cs
@@ -0,0 +1,10 @@
+namespace CourseManager.Domain
+{
+    public class Course
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public int duration { get; set; }
+        public string status { get; set; }
+    }
+}
