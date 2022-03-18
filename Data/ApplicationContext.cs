689632f2e2955a974223fd28c17b289b4957cd32
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-03-17 23:58:32 -0300

    🚀 :rocket: [Add] Insert mock data

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -1,16 +1,22 @@
 using CourseManager.Domain;
 using CourseManager.Data.Configurations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CourseManager.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+        
         public DbSet<Course> Courses { get; set; }
         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=CourseManager;Integrated Security=true");
+            optionsBuilder
+            .UseLoggerFactory(_logger)
+            .EnableSensitiveDataLogging()
+            .UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=CourseManager;Integrated Security=true");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using CourseManager.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +17,88 @@ namespace CourseManager
         {
             using var db = new Data.ApplicationContext();
             
-            db.Database.Migrate();
+            var exists = db.Database.GetPendingMigrations().Any();
+
+            if (exists)
+            {
+
+            };
             
+            // InsertData();
+            // InsertMassiveData();
+            // GetData();
+            UpdateData();
             CreateHostBuilder(args).Build().Run();
+        }
+
+        private static void InsertMassiveData()
+        {
+            var courseList = new[]
+            {
+                new Course
+                {
+                    Title = ".NET Core",
+                    Duration = 10,
+                    Status = "Active"
+                },
+                new Course
+                {
+                    Title = ".NET Core",
+                    Duration = 10,
+                    Status = "Active"
+                }
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.Set<Course>().AddRange(courseList);
+
+            var registries = db.SaveChanges();
+            Console.WriteLine($"All Registry(s): {registries}");
+        }
+ 
+        private static void GetData()
+        {
+            using var db = new Data.ApplicationContext();
+            // var getBySyntax = (from c in db.Courses where c.Id > 0 select c).ToList();
+            var getByMethod = db.Courses
+                .Where(p => p.Id > 0)
+                .OrderBy(p => p.Id)
+                .ToList();
+
+            foreach(var course in getByMethod)
+            {
+                Console.WriteLine($"Getting course: {course.Id}");
+                db.Courses.FirstOrDefault(p => p.Id == course.Id);
+            }
+        }
+
+        private static void UpdateData()
+        {
+            using var db = new Data.ApplicationContext();
+            var course = db.Courses.FirstOrDefault(p => p.Id == 1);
+            course.Name = "Course Changed Step 1";
+
+            db.Courses.Update(course);
+            db.SaveChanges();
+        }
+
+        private static void InsertData()
+        {
+            var course = new Course
+            {
+                Title = "Entity Framework Core",
+                Duration = 10,
+                Status = "Active"
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.Courses.Add(course);
+            db.Set<Course>().Add(course);
+            db.Entry(course).State = EntityState.Added;
+            db.Add(course);
+
+            var registries = db.SaveChanges();
+            Console.WriteLine($"All registry(s): {registries}");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
